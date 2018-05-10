92cf29ce60a0ec292612da9e6e7694420056b836
Mahsa Dilmaghani <mahsadil@icloud.com>
2018-05-10 23:02:03 +0100

    initial build

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express')
+var request = require('request');
+
+var app = express();
+
+// set up template engine
+app.set('view engine', 'ejs');
+
+// static files
+app.use(express.static('./public'));
+
+var url = 'https://www.metaweather.com/api/location/44418/';
+
+app.get('/', function(req, res){
+  request(url, function(error, response, body) {
+    weather_json = JSON.parse(body);
+    console.log(weather_json);
+
+    var weather = {
+      city: weather_json['title'],
+      state: weather_json.consolidated_weather[0].weather_state_name,
+      min_temp: Math.round(weather_json.consolidated_weather[0].min_temp),
+      max_temp: Math.round(weather_json.consolidated_weather[0].max_temp),
+      humidity: weather_json.consolidated_weather[0].humidity
+
+    };
+    var weather_data = {weather : weather};
+    res.render('weather', weather_data);
+  });
+
+});
+
+// listening to port 8000
+app.listen(8000);
